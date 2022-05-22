025fa680191e4cf3c46dea9ebfec72d2f9ad7c90
Kirill Neevin <kirillmisha@gmail.com>
2022-05-22 23:21:10 +0300

    Fix linter

--- pkg/graphics/graph.go
+++ pkg/graphics/graph.go
@@ -28,17 +28,12 @@ func NewGraphHandler(logger *zap.Logger) *graphHandler {
 }
 
 func (h *graphHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	var err error
 
 	if len(r.URL.Path) >= len(detailPath) && r.URL.Path[:len(detailPath)] == detailPath {
 		err = h.handleDetail(w, r)
-		if err != nil {
-
-		}
 	} else {
 		err = h.handleMain(w)
-
 	}
 
 	if err != nil {
@@ -60,18 +55,24 @@ func (h *graphHandler) handleDetail(w http.ResponseWriter, r *http.Request) erro
 
 	file, err := os.Open(graphsPath + path + ".html")
 	if err != nil {
-		_, err = fmt.Fprintln(w, "<body>")
-		_, err = fmt.Fprintln(w, "No graph for this request")
-		_, err = fmt.Fprintln(w, "</body>")
+		_, _ = fmt.Fprintln(w, "<body>")
+		_, _ = fmt.Fprintln(w, "No graph for this request")
+		_, _ = fmt.Fprintln(w, "</body>")
 
 		return xerrors.Errorf("cant find graphic with path: %w", err)
 	}
 
 	b, err := ioutil.ReadAll(file)
-	_, err = fmt.Fprintln(w, string(b))
+	if err != nil {
+		if err = file.Close(); err != nil {
+			return xerrors.Errorf("Can't close file with graphic: %w", err)
+		}
+		return xerrors.Errorf("Can't read file with graphic: %w", err)
+	}
+	_, _ = fmt.Fprintln(w, string(b))
 
 	if err = file.Close(); err != nil {
-		return err
+		return xerrors.Errorf("Can't close file with graphic: %w", err)
 	}
 	return nil
 }
@@ -82,8 +83,8 @@ func (h *graphHandler) handleMain(w http.ResponseWriter) error {
 		return xerrors.Errorf("Can't create dir for graphics: %w", err)
 	}
 
-	_, err = fmt.Fprintln(w, "<body>")
-	_, err = fmt.Fprintln(w, "Welcome to main page<br/><br/>Please select trading bot config name for monitoring:<br/><br/>")
+	_, _ = fmt.Fprintln(w, "<body>")
+	_, _ = fmt.Fprintln(w, "Welcome to main page<br/><br/>Please select trading bot config name for monitoring:<br/><br/>")
 
 	files, err := ioutil.ReadDir(graphsPath)
 	if err != nil {
@@ -93,7 +94,7 @@ func (h *graphHandler) handleMain(w http.ResponseWriter) error {
 	for _, f := range files {
 		if !f.IsDir() && strings.HasSuffix(f.Name(), ".html") {
 			s := fmt.Sprintf("<a href='http://localhost:8080/detail/%s'>%s</a><br/><br/>", f.Name(), f.Name())
-			_, err = fmt.Fprintln(w, s)
+			_, _ = fmt.Fprintln(w, s)
 		}
 	}
 	_, err = fmt.Fprintln(w, "</body>")
